c237d5c1684ec3abd61231d164dbf18d178c8e7a
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-26 10:07:20 +0300

    Creates models.py in app. Writes source and news class

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+
+class News:
+    '''
+    Categorized news class to define News Objects
+    '''
+
+    def __init__(self,Sourcename, author, description, url, urlToImage, publishedAt, content):
+        self.Sourcename = Sourcename
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
